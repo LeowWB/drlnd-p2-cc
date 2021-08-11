c889cc392bb75c214964ca946e227ed071a85ab2
His Royal Majesty <dleowwb@gmail.com>
2021-08-11 15:51:31 +0800

    Various changes including noise (OU -> normal), action rescaling (max 0.33), architecture (simpler); agent now knows to bend elbow instead of flailing round and round

--- model.py
+++ model.py
@@ -22,19 +22,21 @@ class Actor(nn.Module):
         """
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fc1 = nn.Linear(state_size, 256)
-        self.fc2 = nn.Linear(256, action_size)
+        self.fc1 = nn.Linear(state_size, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, action_size)
         self.reset_parameters()
 
     def reset_parameters(self):
-        #self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
-        self.fc1.weight.data.uniform_(-100, 100)
-        self.fc2.weight.data.uniform_(-100, 100)
+        self.fc1.weight.data.uniform_(*hidden_init(self.fc1))
+        self.fc2.weight.data.uniform_(-3e-3, 3e-3)
+        self.fc3.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        return F.tanh(self.fc2(x))
+        x = F.leaky_relu(self.fc1(state))
+        x = F.leaky_relu(self.fc2(x))
+        return F.tanh(self.fc3(x)) * 0.33
 
 
 class Critic(nn.Module):
@@ -50,22 +52,19 @@ class Critic(nn.Module):
         """
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, 256)
-        self.fc2 = nn.Linear(256+action_size, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, 1)
+        self.fcs1 = nn.Linear(state_size, 32)
+        self.fc2 = nn.Linear(32+action_size, 16)
+        self.fc3 = nn.Linear(16, 1)
         self.reset_parameters()
 
     def reset_parameters(self):
         self.fcs1.weight.data.uniform_(*hidden_init(self.fcs1))
         self.fc2.weight.data.uniform_(*hidden_init(self.fc2))
         self.fc3.weight.data.uniform_(*hidden_init(self.fc3))
-        self.fc4.weight.data.uniform_(-3e-3, 3e-3)
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
         xs = F.leaky_relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
         x = F.leaky_relu(self.fc2(x))
-        x = F.leaky_relu(self.fc3(x))
-        return self.fc4(x)
+        return self.fc3(x)
